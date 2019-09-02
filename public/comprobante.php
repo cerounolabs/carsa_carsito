aef70d57422f90fc3a0c73070157bc45cc7b7fa7
Christian Zelaya <christian@cerouno.com.py>
2019-09-02 17:03:35 -0400

    Update v1.21

--- public/comprobante.php
+++ public/comprobante.php
@@ -155,7 +155,7 @@
                     <div class="modal-dialog" role="document">
                         <div class="modal-content">
                             <div class="modal-header" style="background-color:#27464e;">
-                                <h4 class="modal-title" id="modalTitle" style="color:#fff;">Rango de Fechas de Comprobantes</h4>
+                                <h4 class="modal-title" id="modalTitle" style="color:#fff;">Filtros de Comprobantes</h4>
                                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                             </div>
                             <div class="modal-body">
@@ -163,7 +163,7 @@
                                     <div class="form-group">
                                         <label for="recipient-name" class="control-label">Operaciones con Estado:</label>
                                         <select class="custom-select mr-sm-2" id="estadoOpe" name="estadoOpe">
-                                            <option value="1" <?php echo $selecTodos; ?>>Todos</option>
+                                            <option value="1" <?php echo $selecTodos; ?>>Todas</option>
                                             <option value="7" <?php echo $selecActivos; ?>>Activas</option>
                                             <option value="10" <?php echo $selecCancelados; ?>>Canceladas</option>
                                         </select>
