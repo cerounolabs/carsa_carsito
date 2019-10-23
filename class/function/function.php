209e61889be78095d76543869040ef1b2bdf53f9
Christian Zelaya <christian@cerouno.com.py>
2019-10-23 10:48:42 -0300

    Bugs EMAIL

--- class/function/function.php
+++ class/function/function.php
@@ -64,7 +64,7 @@
         try {
             $mail->SMTPDebug    = SMTP::DEBUG_SERVER;
             $mail->isSMTP();
-            $mail->Host         = 'smtp.it.com.py';
+            $mail->Host         = '192.185.195.43';
             $mail->SMTPAuth     = true;
             $mail->Username     = 'czelaya@it.com.py';
             $mail->Password     = 'fxiw~M3Lg%Qp';
