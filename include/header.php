c20cd365da309efc4d7117c0ac26ae0189026379
Christian Zelaya - PC-01 <christian@cerouno.com.py>
2019-10-27 12:41:06 -0300

    Feature HTTPS

--- include/header.php
+++ include/header.php
@@ -2,7 +2,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="description" content="C.A.R.S.A. Sistema Comprobante Web">
-	<meta name="author" content="CEROUN Labs https://cerouno.com.py/ - +595 984 779436">
+	<meta name="author" content="CEROUNO Labs https://cerouno.com.py/ - +595 984 779436">
 	<link href="../assets/images/favicon.png" rel="icon" type="image/png" sizes="16x16">
 	<link href="../assets/libs/chartist/dist/chartist.min.css" rel="stylesheet">
 	<link href="../assets/libs/morris.js/morris.css" rel="stylesheet">
